21cdf263b1a75e627ed27b037bdd9a0f3424cec0
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-07-30 10:33:03 -0700

    users route is uodated

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,14 @@
+
 const express = require('express');
 const { getUsers, createUser, updateUser, deleteUser, getTeachers } = require('../controllers/userController');
+const isAdmin = require('../Middlewares/isAdmin');
+const authenticate = require('../middlewares/authenticate');
 const router = express.Router();
 
-router.get('/', getUsers);
-router.post('/', createUser);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
-router.get('/teachers', getTeachers);
+router.get('/', authenticate, isAdmin, getUsers);
+router.post('/', authenticate, isAdmin, createUser);
+router.put('/:id', authenticate, isAdmin, updateUser);
+router.delete('/:id', authenticate, isAdmin, deleteUser);
+router.get('/teachers', authenticate, isAdmin, getTeachers);
 
 module.exports = router;
