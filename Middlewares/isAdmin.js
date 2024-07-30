4022e6f05f31ad11a7ad5ae3afe04d72c0f4c3f6
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-07-30 10:31:20 -0700

    isAdmin middleware is made so user can see all the data of teahers and login

--- Middlewares/isAdmin.js
+++ Middlewares/isAdmin.js
@@ -1,8 +1,7 @@
 
 module.exports = (req, res, next) => {
-    if (req.user && req.user.role === 'Admin') {
-      return next();
-    }
-    return res.status(403).json({ error: 'Access denied' });
-  };
-  
+  if (req.user && req.user.role === 'Admin') {
+    return next();
+  }
+  return res.status(403).json({ error: 'Access denied' });
+};
